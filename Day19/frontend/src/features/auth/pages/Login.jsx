c49829df6a688fa8fba81114f26251220278aece
Ayaan Ahmad <ayaanah287@gmail.com>
2026-02-19 01:37:02 +0530

    feat: initialize integration

---
+++ Day19/frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import '../style/form.scss'
+import { Link } from 'react-router'
+
+const Login = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        axios.post("http://localhost:3000/api/auth/login",{
+            username,
+            password
+        },{
+            withCredentials:true
+        }).then(res=>{
+            console.log(res.data)
+        })
+    }
+  return (
+    <main>
+        <div className="form-container">
+            <h1>Login</h1>
+            <form onClick={handleSubmit}>
+                <input
+                        onInput={(e) => { setUsername(e.target.value) }}
+                        type="text"
+                        name='username'
+                        placeholder='Enter username' />
+                    <input
+                        onInput={(e) => { setPassword(e.target.value) }}
+                        type="password"
+                        name='password'
+                        placeholder='Enter password' />
+                    <button>Register</button>
+            </form>
+             <p>Don't have an account? <Link className='toggleAuthForm' to="/register">Register</Link></p>
+        </div>
+    </main>
+  )
+}
+
+export default Login
